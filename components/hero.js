74ab3fe818be32bf7d18ba497291c57a581427e4
Ram Refai <ramrefai5@gmail.com>
2023-10-20 18:59:50 -0500

    updating

--- components/hero.js
+++ components/hero.js
@@ -22,7 +22,7 @@ const Hero = () => {
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="/campuses/UTA"
+                href="/campuses/Landing"
                 
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-bermuda rounded-md ">
--- pages/campuses/Landing.js
+++ pages/campuses/Landing.js
@@ -1,10 +1,7 @@
 function Landing() {
   return (
     <>
-    <Navbar/>
       <h1 className="text-center text-bermuda text-3xl">Welcome to the University of Texas at Arlington, Maverick!</h1>
-      <SectionTitle title="yoo"/>
-      <Footer/>
     </>
   );
 }
